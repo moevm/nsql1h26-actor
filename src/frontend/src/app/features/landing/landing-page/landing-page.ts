e81bdee638e77313a0ab53d40a3194ddc1133100
Lapshov Konstantin <kosta-lap@yandex.ru>
2026-03-02 18:34:15 +0300

    Header is ready

--- src/frontend/src/app/features/landing/landing-page/landing-page.ts
+++ src/frontend/src/app/features/landing/landing-page/landing-page.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Footer } from '../../../layout/footer/footer';
+import { Header } from '../../../layout/header/header';
 
 @Component({
   standalone: true,
   selector: 'app-landing-page',
-  imports: [CommonModule, Footer],
+  imports: [CommonModule, Footer, Header],
   templateUrl: './landing-page.html',
   styleUrls: ['./landing-page.scss'],
 })
